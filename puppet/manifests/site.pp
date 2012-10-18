f0ab716cd5118bed0331eec94d649026e1f3d695
Ivo Nellis <ivo.nellis@gmail.com>
2012-10-18 22:14:52 +0300

    deploy

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -7,7 +7,7 @@ node 'default' {
     version => '0.8.8',
   }
 
-  package { ['forever', 'uglify-js']:
+  package { ['forever', 'phantomjs', 'uglify-js']:
     provider => 'npm',
     ensure => 'installed',
   }
