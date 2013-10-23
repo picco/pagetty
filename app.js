e2f239a837ad146b8d535490993e43e8f3e4e288
Ivo Nellis <ivo.nellis@gmail.com>
2013-10-23 20:30:36 +0000

    live cahanges

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-var broadway = require('broadway');
-var app = new broadway.App();
+var cluster = require('cluster');
 
-// Load plugins.
-app.use(require('./plugins/main.js'));
-app.use(require('./plugins/server.js'));
-
-// Launcher.
-app.init(function (err) {
-  if (err) console.log(err);
+/*
+require('nodetime').profile({
+  accountKey: 'c2087d25f074a96e5e842cb25d957f2a58d6cba1',
+  appName: 'Pagetty'
 });
+*/
+
+
+if (cluster.isMaster) {
+  var stamp = new Date().getTime();
+  var worker_env = {build: stamp};
+  var cpus = require('os').cpus().length;
+
+  //cpus = 1;
+
+  console.log('Starting cluster master with ' + cpus + ' workers');
+
+  // Fork workers.
+  for (var i = 0; i < cpus; i++) {
+    cluster.fork(worker_env);
+  }
+
+  // When a worker dies, launch another one.
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('Worker ' + worker.process.pid + ' died, starting another one.');
+    cluster.fork(worker_env);
+  });
+}
+else {
+  console.log('Starting worker #' + cluster.worker.id);
+
+  var broadway = require('broadway');
+  var app = new broadway.App();
+
+  app.worker = cluster.worker;
+  app.build = process.env.build;
+
+  // Load plugins.
+  app.use(require('./plugins/main.js'));
+  app.use(require('./plugins/server.js'));
+
+  // Launcher.
+  app.init(function (err) {
+    if (err) console.log(err);
+  });
+}
