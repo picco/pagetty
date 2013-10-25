46d052664ff492e8f9d5b60d978070e395095323
Ivo Nellis <ivo.nellis@gmail.com>
2013-10-25 20:39:43 +0000

    search 100 items max

--- models/item.js
+++ models/item.js
@@ -61,7 +61,7 @@ exports.attach = function(options) {
         });
       }
       else if (list.type == "search") {       
-        es.search({query: {query_string: {query: variant}}, from: page * app.conf.load_items, size: app.conf.load_items}, function(err, data) {
+        es.search({query: {query_string: {query: variant}}, from: 0, size: 100}, function(err, data) {
           if (err) {
             next();
           }
